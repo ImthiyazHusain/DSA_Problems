e1b4f62f671833c83a7779c64e7095c37b56eae3
imthiyaazhusain <imthiyazhusain02.09.2005@gmail.com>
2026-01-26 21:55:06 +0530

    palindrome

---
+++ src/Recursion/Strings/palindrome.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class palindrome{
+	public static void main(String[] args) {
+		String s = "HellolleH";
+		System.out.print(palin(s));
+	}
+public static boolean palin(String s){
+	    if(s.length()<=1) return true;
+	    
+	    char first = s.charAt(0);
+	    char last = s.charAt(s.length()-1);
+	    String mid = s.substring(1,s.length()-1);
+	    
+	    return first == last && palin(mid);
+	}
+}
---
+++ src/Recursion/mathSums/palindrome.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class palindrome{
+	public static void main(String[] args) {
+		int n = 121219;
+        if(n<0) n = n*-1;
+		System.out.print(palin(n));
+	}
+	public static boolean palin(int n){
+	    if(n<10) return true;
+	    
+	    int first = n/(int)(Math.pow(10,(int)Math.log10(n/10)+1));
+	    int last = n%10;
+	    int mid = (n-((int)(Math.pow(10,(int)Math.log10(n/10)+1)))*first)/10;
+	    
+	    return first == last && palin(mid);
+	}
+}
