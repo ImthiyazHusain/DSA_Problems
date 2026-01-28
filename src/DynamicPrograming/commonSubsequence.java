359a99accb504e5856051ebfc2bb294181191023
Imthiyaz Husain <imthiyazhusain02.09.2005@gmail.com>
2026-01-28 14:33:30 +0530

    DP

--- src/DynamicPrograming/commonSubsequence.java
+++ src/DynamicPrograming/commonSubsequence.java
@@ -8,25 +8,19 @@ public class commonSubsequence {
         String s1 = "Husain";
         String s2 = "husain";
 
-        String dp[][] = new String[s2.length()+1][s1.length()+1];
+        String dp[][] = new String[s1.length()+1][s2.length()+1];
         for(String[] arr : dp){
             Arrays.fill(arr,"");
         }
-        for(int i=1;i<=s2.length();i++){
-            for(int j=1;j<=s1.length();j++){
-                if(s2.charAt(i-1)==s1.charAt(j-1)){
+        for(int i=1;i<=s1.length();i++){
+            for(int j=1;j<=s2.length();j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1]+s1.charAt(i-1);
                 }else{
                     dp[i][j] = (dp[i-1][j].length()<=dp[i][j-1].length()?dp[i][j-1]:dp[i-1][j]);
                 }
             }
         }
-//        for(String[] arr : dp){
-//            for(String s : arr){
-//                System.out.print(s+" ");
-//            }
-//            System.out.println();
-//        }
          System.out.print(dp[dp.length-1][dp[0].length-1]);
     }
 }
